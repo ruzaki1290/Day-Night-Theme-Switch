0520b74ea1e58147801d8b9b8a51607f658414e7
Rus <rus.zakirov@triosstudent.com>
2024-08-08 22:40:46 -0400

    Added properties in app.css and app.js to make the sun and moon rotate

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,10 @@
+const dayNightContainer = document.querySelector('.day-night-container')
+
 // Toggles between dark and light themes
 document.querySelector('.theme-switch-btn').addEventListener
 ('click', () => {
    document.body.classList.toggle('dark');
+   const currentRotation = parseInt(getComputedStyle(dayNightContainer)
+   .getPropertyValue('--rotation'))
+   dayNightContainer.style.setProperty('--rotation', currentRotation + 180)
 });
